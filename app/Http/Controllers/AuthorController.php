35c4a89e2e9ee4f082b0402b71826159dce1ac51
= <lacerda.cassio@gmail.com>
2021-03-21 11:06:15 +0000

    move cpu

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Author;
+use Illuminate\Http\Request;
+
+namespace App\Http\Controllers;
+
+class AuthorController extends Controller
+{
+
+
+    public function index()
+    {
+        return 'Micro Serviço Authors';
+    }
+
+
+
+}
