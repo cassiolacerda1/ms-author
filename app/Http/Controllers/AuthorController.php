549752547669a63f74f1c3aca71fd025c66b5fbc
= <lacerda.cassio@gmail.com>
2021-03-24 09:47:21 +0000

    authors working

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -1,19 +1,94 @@
 <?php
 
-use App\Author;
-use Illuminate\Http\Request;
-
 namespace App\Http\Controllers;
+
+use App\Author;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AuthorController extends Controller
 {
+    use ApiResponser;
 
+    //------------------------------------------------------------------------------------------------------------------
+
+    public function __construct()
+    {
+        //
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
 
     public function index()
     {
-        return 'Micro Serviço Authors';
+        $authors = Author::all();
+
+        return $this->successResponse($authors);
     }
 
+    //------------------------------------------------------------------------------------------------------------------
 
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255|in:male,female',
+            'country' => 'required|max:255',
+        ];
+
+        $this->validate($request, $rules);
+
+        $author = Author::create($request->all());
+
+        return $this->successResponse($author, Response::HTTP_CREATED);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    public function show($author)
+    {
+        $author = Author::findOrFail($author);
+
+        return $this->successResponse($author);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    public function update(Request $request, $author)
+    {
+        $rules = [
+            'name' => 'max:255',
+            'gender' => 'max:255|in:male,female',
+            'country' => 'max:255',
+        ];
+
+        $this->validate($request, $rules);
+
+        $author = Author::findOrFail($author);
+
+        $author->fill($request->all());
+
+        if ($author->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $author->save();
+
+        return $this->successResponse($author);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    function destroy($author)
+    {
+        $author = Author::findOrFail($author);
+
+        $author->delete();
+
+        return $this->successResponse($author);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
 
 }
